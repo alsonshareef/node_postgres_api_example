b8e6d2133a32c4b553266a7cba9469c5ffb5c553
Alson Shareef <alson.shareef@gmail.com>
2020-03-31 19:13:43 +0800

    Imported database query methods and attached to http routes.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const db = require('./database/queries');
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -12,6 +14,12 @@ app.use(
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' });
 });
+
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
